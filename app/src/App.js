173492f08a6870d8bf687c967dd07acadbcf5f94
Chris Lane <chris.lane359@gmail.com>
2018-11-30 07:34:20 -0800

    Added more components and fixed some syntax issues

--- app/src/App.js
+++ app/src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import "./App.css";
 import Component1 from "./Component1.js";
 import Component2 from "./Component2.js";
+import Component3 from "./Component3.js";
+import Component4 from "./Component4.js";
 
 import { BrowserRouter, Route, Redirect, Switch, Link } from "react-router-dom";
 
@@ -17,11 +19,15 @@ class App extends Component {
             <ul role="navigation">
               <li><Link to="Component1">Component 1</Link></li>
               <li><Link to="Component2">Component 2</Link></li>
+              <li><Link to="Component3">Component 3</Link></li>
+              <li><Link to="Component4">Component 4</Link></li>
             </ul>
 
             <Switch>
               <Route path="/Component1" component={Component1} />
               <Route path="/Component2" component={Component2} />
+              <Route path="/Component3" component={Component3} />
+              <Route path="/Component4" component={Component4} />
             </Switch>
           </div>
         </BrowserRouter>
