c33a138ba9bfdc399fa5a7f127ac69bf7db9615c
Chris Lane <chris.lane359@gmail.com>
2018-12-09 10:31:46 -0800

    Added landmark roles, updated routing link styles for mobile and formatting

--- app/src/App.js
+++ app/src/App.js
@@ -11,31 +11,35 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Basic Routing in React</h1>
 
-        <div>
-          <a href="https://github.com/clane/React-basic-routing">
-            https://github.com/clane/React-basic-routing
-          </a>
+        <div role="banner" aria-label="routing time">
+          <h1>Basic Routing in React</h1>
+          <div>
+            <a href="https://github.com/clane/React-basic-routing">
+              https://github.com/clane/React-basic-routing
+            </a>
+          </div>
         </div>
 
         <BrowserRouter>
           <div>
 
-            <ul role="navigation">
+            <ul role="navigation" aria-label="primary navigation">
               <li><Link to="Component1">Component 1</Link></li>
               <li><Link to="Component2">Component 2</Link></li>
               <li><Link to="Component3">Component 3</Link></li>
               <li><Link to="Component4">Component 4</Link></li>
             </ul>
+            <div role="main" aria-label="components go here">
+              <Switch>
+                <Route path="/Component1" component={Component1} />
+                <Route path="/Component2" component={Component2} />
+                <Route path="/Component3" component={Component3} />
+                <Route path="/Component4" component={Component4} />
+                <Redirect from="/" to="/Component1" />
+              </Switch>
+            </div>
 
-            <Switch>
-              <Route path="/Component1" component={Component1} />
-              <Route path="/Component2" component={Component2} />
-              <Route path="/Component3" component={Component3} />
-              <Route path="/Component4" component={Component4} />
-              <Redirect from="/" to="/Component1" />
-            </Switch>
           </div>
         </BrowserRouter>
 
